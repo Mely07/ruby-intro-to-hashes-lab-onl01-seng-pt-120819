392404a75f1901d603857d1ccf78992f49b1402b
Damely Tineo <damely.tineo07@gmail.com>
2019-12-22 07:59:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,8 +29,12 @@ def monopoly_with_third_tier
       :railroads=>{
         :pieces=>4,
         :names=>{
-          :reading_railroad=>{}
-          :pennsylvania_railroad=>{}
+          :reading_railroad=>{
+            
+          }
+          :pennsylvania_railroad=>{
+            
+          }
         },
         :rent_in_dollars=>{
           :one_piece_owned=>25,
